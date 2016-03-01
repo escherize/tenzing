7963ef35324651ec1f14e46583c9d1f753566523
Bryan Maass <bryan.maass@gmail.com>
2016-03-02 09:35:56 +1100

    add +cider option, and accompanying cider-dev task

--- src/leiningen/new/tenzing.clj
+++ src/leiningen/new/tenzing.clj
@@ -69,6 +69,9 @@
 (defn test? [opts]
   (some #{"+test"} opts))
 
+(defn cider? [opts]
+  (some #{"+cider"} opts))
+
 ;; ---------------------------------------------------------------
 ;; Template data helpers
 ;; ---------------------------------------------------------------
@@ -115,9 +118,28 @@
         (watch)
         (test-cljs :js-env :phantom)))")
 
+(def cider-tasks
+"(deftask cider \"CIDER profile\"
+[]
+(require 'boot.repl)
+(swap! @(resolve 'boot.repl/*default-dependencies*)
+       concat '[[org.clojure/tools.nrepl \"0.2.12\"]
+                [cider/cider-nrepl \"0.10.2\"]
+                [refactor-nrepl \"2.0.0\"]])
+(swap! @(resolve 'boot.repl/*default-middleware*)
+       concat '[cider.nrepl/cider-middleware
+                refactor-nrepl.middleware/wrap-refactor])
+identity)
+
+(deftask cider-dev
+  \"Adds cider middleware to the dev task.\"
+  []
+  (comp (cider) (repl) (dev)))")
+
 (defn build-tasks [opts]
   (cond-> []
-          (test? opts) (conj test-tasks)))
+          (test? opts) (conj test-tasks)
+          (cider? opts) (conj cider-tasks)))
 
 ;; (defn pre-build-steps [name opts]
 ;;   (cond-> []
